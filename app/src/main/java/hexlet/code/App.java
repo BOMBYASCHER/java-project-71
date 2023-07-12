b27276454b975e25fadf4bdae23824467b7f6006
BOMBYASCHER <bombyascher@gmail.com>
2023-07-12 17:45:12 +0300

    Update CLI

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -8,6 +8,12 @@ import picocli.CommandLine.Parameters;
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "1.0.0",
         description = "Compares two configuration files and shows a difference.")
 public class App implements Runnable {
+    @Parameters(description = "path to first file")
+    private String filepath1;
+    @Parameters(description = "path to second file")
+    private String filepath2;
+    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]", defaultValue = "stylish")
+    private String format;
     @Override
     public void run() {
 
